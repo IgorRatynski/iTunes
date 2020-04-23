66bb155e0440868dd78cede67647abb674fd599b
Igor Ratynski <igor.ratynski@gmail.com>
2020-04-23 18:01:50 +0300

    added api response models

---
+++ iTunesTest/Model/ITunesResponseModel.swift
@@ -0,0 +1,14 @@
+//
+//  ITunesResponseModel.swift
+//  iTunesTest
+//
+//  Created by Igor Ratynski on 23.04.2020.
+//  Copyright © 2020 Igor Ratynski. All rights reserved.
+//
+
+import Foundation
+
+struct ITunesResponseModel {
+    let resultCount: Int
+    let results: [ITunesArtistModel]
+}
