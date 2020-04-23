b0aa0c34551bac5764f8aa6dda62ec9f20033e8c
Igor Ratynski <igor.ratynski@gmail.com>
2020-04-23 18:01:18 +0300

    added moya request extension

---
+++ iTunesTest/Extension/Moya+Request.swift
@@ -0,0 +1,16 @@
+//
+//  Moya+Request.swift
+//  iTunesTest
+//
+//  Created by Igor Ratynski on 23.04.2020.
+//  Copyright © 2020 Igor Ratynski. All rights reserved.
+//
+
+import Moya
+import ReactiveSwift
+
+public extension MoyaProvider {
+    func request(_ targetType: Target) -> SignalProducer<Response, MoyaError> {
+        return reactive.request(targetType).observe(on: UIScheduler())
+    }
+}
