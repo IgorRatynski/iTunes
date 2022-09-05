0467fade9d3464bc43b4d014a0b4f3034c4b210e
igor ratynski <igor.ratynski@gmail.com>
2022-09-05 04:08:17 +0300

    changed alert config

--- iTunesTest/Model/Alert/AlertConfig.swift
+++ iTunesTest/Model/Alert/AlertConfig.swift
@@ -11,12 +11,16 @@ import UIKit
 struct AlertConfig: AlertConfigProtocol {
   let title: String?
   let message: String?
+  let messageForUser: String?
+  let isVisibleForUser: Bool
   
   let actions: [AlertAction]?
   
-  init(title: String?, message: String?, actions: [AlertAction]? = nil) {
+  init(title: String?, message: String?, messageForUser: String? = nil, isVisibleForUser: Bool = false, actions: [AlertAction]? = nil) {
     self.title = title
     self.message = message
+    self.messageForUser = message
+    self.isVisibleForUser = isVisibleForUser
     self.actions = actions
   }
 }
