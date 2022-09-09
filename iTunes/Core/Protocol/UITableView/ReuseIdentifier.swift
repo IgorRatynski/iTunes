9c2f6b3ac0572b5e95fbf40aabff5804961b3492
igor ratynski <igor.ratynski@gmail.com>
2022-09-10 02:57:48 +0300

    moved uitableviewcell extension

--- iTunes/Core/Protocol/UITableView/ReuseIdentifier.swift
+++ iTunes/Core/Protocol/UITableView/ReuseIdentifier.swift
@@ -11,11 +11,3 @@ import UIKit
 protocol ReuseIdentifying {
   static var reuseIdentifier: String { get }
 }
-
-extension ReuseIdentifying {
-  static var reuseIdentifier: String {
-    return String(describing: Self.self)
-  }
-}
-
-extension UITableViewCell: ReuseIdentifying { }
